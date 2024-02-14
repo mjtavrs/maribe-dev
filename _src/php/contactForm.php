a436a6202dbdb7a1f189abe0db849121c1e26e0f
Marcos Tavares <mjtdes.md@gmail.com>
2024-02-14 11:13:46 -0300

    feat: create php file for testing the contact form

---
+++ _src/php/contactForm.php
@@ -0,0 +1,24 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $telefone = $_POST["telefone"];
+    $assunto = $_POST["assunto"];
+    $mensagem = $_POST["mensagem"];
+
+    $to = "mjtdes.md@gmail.com";
+    $assunto = "Teste de contato";
+    $mensagem_email = "Nome: $nome\n";
+    $mensagem_email .= "E-mail: $email\n";
+    $mensagem_email .= "Telefone: $telefone\n";
+    $mensagem_email .= "Assunto: $assunto\n";
+    $mensagem_email .= "Mensagem: \n$mensagem\n";
+
+    $headers = "De: $email\r\nReply-To: $email\r\n";
+
+    if (mail($to, $assunto, $mensagem_email, $headers)) {
+        echo 'Mensagem enviada com sucesso!';
+    } else {
+        echo 'Erro ao enviar a mensagem.';
+    }
+}
