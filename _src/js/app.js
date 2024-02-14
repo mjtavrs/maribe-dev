33552506b78b42037e5093e8e32f16a9974f3a49
Marcos Tavares <mjtdes.md@gmail.com>
2024-02-14 15:19:16 -0300

    feat: finish the 404 page and change the emoji inside the footer

--- _src/js/app.js
+++ _src/js/app.js
@@ -3,5 +3,10 @@ document.addEventListener("DOMContentLoaded", () => {
         if (e && e.target && e.target.tagName === "img") {
             window.location.href = "404.html";
         }
-    })
+    });
+
+    // if (window.location.pathname.endsWith(".html")) {
+    //     const newPath = window.location.pathname.slice(0, -5);
+    //     history.pushState({}, "", newPath);
+    // }
 })
