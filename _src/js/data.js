cb2e8dc973a3f2fff88c6219b292f18bc973d767
Marcos Tavares <mjtdes.md@gmail.com>
2024-02-15 13:13:08 -0300

    feat: include the projects to be shown at the projects page, update the data.js with the new pictures

--- _src/js/data.js
+++ _src/js/data.js
@@ -1,29 +1,84 @@
 const projetos = [
     {
         id: 1,
+        url: "_assets/image/projetos/projeto-marias/projeto-marias-cover.jpg",
         titulo: "projeto marias",
-        url: "_assets/image/projetos/projeto-marias/projeto_marias_cover.jpg"
+        descricao: "",
+        ano: "",
+        cidade: "Recife"
     },
     {
         id: 2,
+        url: "_assets/image/projetos/quarto-selva/quarto-selva-cover.png",
+        titulo: "quarto selva",
+        descricao: "",
+        ano: "",
+        cidade: "Recife"
+    },
+    {
+        id: 3,
+        url: "_assets/image/projetos/ape-eron/ape-eron-cover.png",
+        titulo: "apê eron",
+        descricao: "",
+        ano: "",
+        cidade: "Recife"
+    },
+    {
+        id: 4,
+        url: "_assets/image/projetos/ape-varandas/ape-varandas-cover.png",
+        titulo: "apê varandas",
+        descricao: "",
+        ano: "",
+        cidade: "Recife"
+    },
+    {
+        id: 5,
+        url: "_assets/image/projetos/agencia-166/agencia-166-cover.jpg",
+        titulo: "agência 166",
+        descricao: "",
+        ano: "",
+        cidade: "Recife"
+    },
+    {
+        id: 6,
+        url: "_assets/image/projetos/ape-nina/ape-nina-cover.jpg",
+        titulo: "apê nina",
+        descricao: "",
+        ano: "",
+        cidade: "Recife"
+    },
+    {
+        id: 7,
+        url: "_assets/image/projetos/ape-pilar/ape-pilar-cover.png",
+        titulo: "apê pilar",
+        descricao: "",
+        ano: "",
+        cidade: "Recife"
+    },
+    {
+        id: 8,
+        url: "_assets/image/projetos/ape-girassol/ape-girassol-cover.jpg",
         titulo: "apê girassol",
-        url: "_assets/image/projetos/ape-girassol/ape_girassol_cover.jpg"
+        descricao: "",
+        ano: "",
+        cidade: "Recife"
     },
     {
-        id: 3,
-        titulo: "apê nina",
-        url: "_assets/image/projetos/ape-nina/ape_nina_cover.jpg"
+        id: 9,
+        url: "_assets/image/projetos/quarto-tete/quarto-tete-cover.png",
+        titulo: "quarto tête",
+        descricao: "",
+        ano: "",
+        cidade: "Recife"
     },
     {
-        id: 3,
-        titulo: "apê nina",
-        url: "_assets/image/projetos/ape-nina/ape_nina_cover.jpg"
-    },
-    {
-        id: 3,
-        titulo: "apê nina",
-        url: "_assets/image/projetos/ape-nina/ape_nina_cover.jpg"
-    },
+        id: 10,
+        url: "_assets/image/projetos/ape-amazonita/ape-amazonita-cover.png",
+        titulo: "apê amazonita",
+        descricao: "",
+        ano: "",
+        cidade: "Recife"
+    }
 ]
 
 export default projetos;
