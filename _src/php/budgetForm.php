e65cd007c209597b47d36f60f24c942cb7ab0335
Marcos Tavares <mjtdes.md@gmail.com>
2024-02-17 11:47:11 -0300

    feat: finish the success page, edit the head of the 404 page, make some checkings on the php scripts to improve the data to be sent

--- _src/php/budgetForm.php
+++ _src/php/budgetForm.php
@@ -1,12 +1,55 @@
 <?php
+
+$erros = [];
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nome = $_POST["name"];
-    $email = $_POST["email"];
+
+    if (empty($_POST["name"])) {
+        $erros[] = "Por favor, digite seu nome.";
+    } else {
+        $nome = $_POST["name"];
+    }
+
+    if (empty($_POST["email"])) {
+        $erros[] = "Por favor, digite seu e-mail.";
+    } elseif (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+        $erros[] = "Formato de e-mail inválido.";
+    } else {
+        $email = $_POST["email"];
+    }
+
     $telefone = $_POST["phone"];
-    $de_onde_veio = $_POST["de-onde-veio"];
-    $o_que_projetar = $_POST["o-que-projetar"];
-    $quando_comecar_projeto = $_POST["quando-comecar-projeto"];
-    $objetivo = $_POST["objetivo"];
+    if (!empty($telefone)) {
+        $telefone = preg_replace('/\D/', '', $telefone);
+
+        if (!preg_match('/^\d{10,11}$/', $telefone)) {
+            $erros[] = "Formato de telefone inválido. Por favor, insira um número de telefone válido.";
+        }
+    }
+
+    if (empty($_POST["de-onde-veio"])) {
+        $erros[] = "Por favor, selecione de onde você veio.";
+    } else {
+        $de_onde_veio = $_POST["de-onde-veio"];
+    }
+
+    if (empty($_POST["o-que-projetar"])) {
+        $erros[] = "Por favor, selecione o que será projetado.";
+    } else {
+        $o_que_projetar = $_POST["o-que-projetar"];
+    }
+
+    if (empty($_POST["quando-comecar-projeto"])) {
+        $erros[] = "Por favor, indique quando pretende iniciar o projeto.";
+    } else {
+        $quando_comecar_projeto = $_POST["quando-comecar-projeto"];
+    }
+
+    if (empty($_POST["objetivo"])) {
+        $erros[] = "Por favor, descreva o objetivo do projeto.";
+    } else {
+        $objetivo = $_POST["objetivo"];
+    }
 
     $to = "mjtdes.md@gmail.com";
     $assunto = "Novo pedido de orçamento";
@@ -21,8 +64,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $headers = "De: $email\r\nReply-To: $email\r\n";
 
     if (mail($to, $assunto, $mensagem_email, $headers)) {
-        echo 'Mensagem enviada com sucesso!';
+        header("Location: sucesso.html");
+        exit;
     } else {
-        echo 'Erro ao enviar a mensagem.';
+        $erros[] = 'Erro ao enviar a mensagem.';
     }
 }
--- _src/php/contactForm.php
+++ _src/php/contactForm.php
@@ -1,10 +1,43 @@
 <?php
+
+$erros = [];
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nome = $_POST["name"];
-    $email = $_POST["email"];
+
+    if (empty($_POST["name"])) {
+        $erros[] = "Por favor, digite seu nome.";
+    } else {
+        $nome = $_POST["name"];
+    }
+
+    if (empty($_POST["email"])) {
+        $erros[] = "Por favor, digite seu e-mail.";
+    } elseif (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+        $erros[] = "Formato de e-mail inválido.";
+    } else {
+        $email = $_POST["email"];
+    }
+
     $telefone = $_POST["phone"];
-    $assunto = $_POST["subject"];
-    $mensagem = $_POST["message"];
+    if (!empty($telefone)) {
+        $telefone = preg_replace('/\D/', '', $telefone);
+
+        if (!preg_match('/^\d{10,11}$/', $telefone)) {
+            $erros[] = "Formato de telefone inválido. Por favor, insira um número de telefone válido.";
+        }
+    }
+
+    if (empty($_POST["subject"])) {
+        $erros[] = "Por favor, escreva o assunto.";
+    } else {
+        $assunto = $_POST["subject"];
+    }
+
+    if (empty($_POST["message"])) {
+        $erros[] = "Por favor, escreva a mensagem do projeto.";
+    } else {
+        $mensagem = $_POST["message"];
+    }
 
     $to = "mjtdes.md@gmail.com";
     $assunto = "Novo contato";
@@ -17,8 +50,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $headers = "De: $email\r\nReply-To: $email\r\n";
 
     if (mail($to, $assunto, $mensagem_email, $headers)) {
-        echo 'Mensagem enviada com sucesso!';
+        header("Location: sucesso.html");
+        exit;
     } else {
-        echo 'Erro ao enviar a mensagem.';
+        $erros[] = 'Erro ao enviar a mensagem.';
     }
 }
