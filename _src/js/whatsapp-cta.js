7e67b9c46eb6222066779b742cf2739dc15e2125
Marcos Tavares <mjtdes.md@gmail.com>
2024-05-03 10:44:58 -0300

    refactor: Refactor the WhatsApp CTA button, add an animation to make it appear more smoothly.

--- _src/js/whatsapp-cta.js
+++ _src/js/whatsapp-cta.js
@@ -1,5 +1,7 @@
 const whatsAppCta = document.querySelector(".whatsapp-cta");
-const ctaText = document.querySelector(".whatsapp-cta a p");
+// const ctaText = document.querySelector(".whatsapp-cta a p");
+const textContainer = document.querySelector(".text-container");
+const iconContainer = document.querySelector(".icon-container");
 let lastScrollPosition = 0;
 
 function handleScroll() {
@@ -9,7 +11,6 @@ function handleScroll() {
         whatsAppCta.classList.add("mostrar");
         whatsAppCta.classList.contains("esconder") ? whatsAppCta.classList.remove("esconder") : ""
     } else if (currentScrollPosition < lastScrollPosition && currentScrollPosition < 100) {
-        whatsAppCta.classList.contains("diminuir") ? whatsAppCta.classList.remove("diminuir") : ""
         whatsAppCta.classList.remove("mostrar");
         whatsAppCta.classList.add("esconder");
     }
@@ -18,15 +19,20 @@ function handleScroll() {
 }
 
 whatsAppCta.addEventListener("mouseover", () => {
-    whatsAppCta.classList.contains("diminuir") ? whatsAppCta.classList.remove("diminuir") : ""
-    whatsAppCta.classList.add("aumentar");
-    ctaText.classList.remove("hidden");
+    iconContainer.classList.contains("slow-remove-border-radius") ? iconContainer.classList.remove("slow-remove-border-radius") : "";
+    textContainer.classList.contains("unscale-in-right") ? textContainer.classList.remove("unscale-in-right") : "";
+    textContainer.classList.remove("hidden");
+    textContainer.classList.add("scale-in-right");
+    iconContainer.classList.add("slow-border-radius");
+    textContainer.classList.add("shown");
 });
 
 whatsAppCta.addEventListener("mouseout", () => {
-    whatsAppCta.classList.remove("aumentar");
-    whatsAppCta.classList.add("diminuir");
-    ctaText.classList.add("hidden");
+    textContainer.classList.add("unscale-in-right");
+    textContainer.classList.add("hidden");
+    textContainer.classList.remove("shown");
+    iconContainer.classList.add("slow-remove-border-radius");
+    iconContainer.classList.remove("slow-border-radius");
 });
 
 window.addEventListener("scroll", handleScroll);
